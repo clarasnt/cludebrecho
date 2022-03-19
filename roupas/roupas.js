da2adbce2e44f76e5f24a455653667f4b1d8bf08
clarasnt <83384345+clarasnt@users.noreply.github.com>
2022-03-19 19:03:23 -0300

    Add files via upload

---
+++ roupas/roupas.js
@@ -0,0 +1,68 @@
+var categorias = document.querySelectorAll(".categorias")
+var menu = document.querySelector("#menu")
+var menus = document.querySelectorAll(".menu")
+var menu_texto = document.querySelector("#menu_texto")
+
+setTimeout(() => {
+    categorias[0].setAttribute("style", 'opacity: 1')
+    menu.setAttribute("style", 'opacity: 1')
+    menu_texto.setAttribute("style", 'opacity: 1')
+    setTimeout(() => {
+        categorias[1].setAttribute("style", 'opacity: 1')
+        setTimeout(() => {
+            categorias[2].setAttribute("style", 'opacity: 1')
+            setTimeout(() => {
+                categorias[3].setAttribute("style", 'opacity: 1')
+                setTimeout(() => {
+                    categorias[4].setAttribute("style", 'opacity: 1')
+                }, 250);
+            }, 250);
+        }, 250);
+    }, 250);
+}, 50);
+
+categorias[0].addEventListener("mouseover", function(){
+    categorias[0].setAttribute("style", "height: 123px; width: 769px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+categorias[0].addEventListener("mouseout", function(){
+    categorias[0].setAttribute("style", "height: 103px; width: 649px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+categorias[1].addEventListener("mouseover", function(){
+    categorias[1].setAttribute("style", "height: 180px; width: 488px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+categorias[1].addEventListener("mouseout", function(){
+    categorias[1].setAttribute("style", "height: 160px; width: 428px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+categorias[2].addEventListener("mouseover", function(){
+    categorias[2].setAttribute("style", "height: 121px; width: 428px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+categorias[2].addEventListener("mouseout", function(){
+    categorias[2].setAttribute("style", "height: 101px; width: 368px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+categorias[3].addEventListener("mouseover", function(){
+    categorias[3].setAttribute("style", "height: 127px; width: 598px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+categorias[3].addEventListener("mouseout", function(){
+    categorias[3].setAttribute("style", "height: 112px; width: 538px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+menu.addEventListener("mouseover", function(){
+    menus[0].setAttribute("style", "opacity: 1")
+    menus[1].setAttribute("style", "opacity: 1")
+    menu_texto.setAttribute("style", "opacity: 0")
+    menu.setAttribute("style", "height: 800px; top: 50px; right:10px; width: 200px; opacity: 1")
+})
+
+menu.addEventListener("mouseout", function(){
+    menus[0].setAttribute("style", "opacity: 0")
+    menus[1].setAttribute("style", "opacity: 0")
+    menu_texto.setAttribute("style", "opacity: 1")
+    menu.setAttribute("style", "height: 500px; top: 200px; width: 60px; opacity: 1")
+})
---
+++ roupas/todos/todos.js
@@ -0,0 +1,85 @@
+var produtos = document.querySelectorAll(".produtos")
+var menu = document.querySelector("#menu")
+var menus = document.querySelectorAll(".menu")
+var menu_texto = document.querySelector("#menu_texto")
+
+setTimeout(() => {
+    produtos[0].setAttribute("style", 'opacity: 1')
+    menu.setAttribute("style", 'opacity: 1')
+    menu_texto.setAttribute("style", 'opacity: 1')
+    setTimeout(() => {
+        produtos[1].setAttribute("style", 'opacity: 1')
+        setTimeout(() => {
+            produtos[3].setAttribute("style", 'opacity: 1')
+            
+        }, 250);
+    }, 250);
+}, 50);
+
+produtos[0].addEventListener("mouseover", function(){
+    produtos[0].setAttribute("style", "height: 170px; width: 468px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+produtos[0].addEventListener("mouseout", function(){
+    produtos[0].setAttribute("style", "height: 160px; width: 428px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+produtos[2].addEventListener("mouseover", function(){
+    produtos[1].setAttribute("style", "height: 460px; width: 370px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+    produtos[2].setAttribute("style", "height: 270px; width: 340px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+produtos[2].addEventListener("mouseout", function(){
+    produtos[1].setAttribute("style", "height: 400px; width: 320px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+    produtos[2].setAttribute("style", "height: 250px; width: 270px; opacity: 0; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+produtos[4].addEventListener("mouseover", function(){
+    produtos[3].setAttribute("style", "height: 460px; width: 370px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+    produtos[4].setAttribute("style", "height: 270px; width: 300px; opacity: 1; background-image: linear-gradient(to left, transparent, white);")
+})
+
+produtos[4].addEventListener("mouseout", function(){
+    produtos[3].setAttribute("style", "height: 400px; width: 320px; opacity: 1; background-image: linear-gradient(to left, transparent, transparent);")
+    produtos[4].setAttribute("style", "height: 250px; width: 270px; opacity: 0; background-image: linear-gradient(to left, transparent, transparent);")
+})
+
+menu.addEventListener("mouseover", function(){
+    menus[0].setAttribute("style", "opacity: 1")
+    menus[1].setAttribute("style", "opacity: 1")
+    menu_texto.setAttribute("style", "opacity: 0")
+    menu.setAttribute("style", "height: 800px; top: 50px; right:10px; width: 200px; opacity: 1")
+})
+
+menu.addEventListener("mouseout", function(){
+    menus[0].setAttribute("style", "opacity: 0")
+    menus[1].setAttribute("style", "opacity: 0")
+    menu_texto.setAttribute("style", "opacity: 1")
+    menu.setAttribute("style", "height: 500px; top: 200px; width: 60px; opacity: 1")
+})
+
+menus[1].addEventListener("mouseover", function(){
+    menus[2].setAttribute("style", "opacity: 1")
+    menus[3].setAttribute("style", "opacity: 1")
+    menus[4].setAttribute("style", "opacity: 1")
+    categorias.setAttribute("style", "opacity: 1")
+    categorias.addEventListener("mouseover", function(){
+        menus[2].setAttribute("style", "opacity: 1")
+        menus[3].setAttribute("style", "opacity: 1")
+        menus[4].setAttribute("style", "opacity: 1")
+        categorias.setAttribute("style", "opacity: 1")
+    })
+    categorias.addEventListener("mouseout", function(){
+        menus[2].setAttribute("style", "opacity: 0")
+        menus[3].setAttribute("style", "opacity: 0")
+        menus[4].setAttribute("style", "opacity: 0")
+        categorias.setAttribute("style", "opacity: 0")
+    })
+})
+
+menus[1].addEventListener("mouseout", function(){
+    menus[2].setAttribute("style", "opacity: 0")
+    menus[3].setAttribute("style", "opacity: 0")
+    menus[4].setAttribute("style", "opacity: 0")
+    categorias.setAttribute("style", "opacity: 0")
+})
